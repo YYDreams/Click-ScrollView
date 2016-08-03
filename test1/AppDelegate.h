b9b5180473d2a48eb9870be3d36d820eb2ab2d5c
YYDreams <799431668@qq.com>
2016-08-03 09:46:20 +0800

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by LOVE on 16/8/3.
+//  Copyright © 2016年 LOVE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
